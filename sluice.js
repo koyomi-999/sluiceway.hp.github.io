37dbfc96ffd3883f53462fb98c55f8400f4cc1e9
koyomi-999 <62170496+koyomi-999@users.noreply.github.com>
2020-04-19 23:56:31 +0900

    Create sluice.js

---
+++ sluice.js
@@ -0,0 +1,21 @@
+$(function(){
+  $('#in-top').click(function(){  
+    $('html, body').animate({
+      'scrollTop':0
+    },'500');
+  });
+
+
+  $('#in-about').click(function(){  
+    $('html, body').animate({
+      'scrollTop':520
+    },'500');
+  });
+
+  $('#in-memb').click(function(){  
+    $('html, body').animate({
+      'scrollTop':735
+    },'500');
+  });
+});
+
